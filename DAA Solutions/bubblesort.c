d0cbabc308804c227c536d24eb81f9570f3553bb
Arka Khatua <arkhakhatua@gmail.com>
2025-04-08 20:46:13 +0530

    Add files via upload

---
+++ DAA Solutions/bubblesort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int complex = 0;
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            complex++;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                complex++;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("The Complexity is: %d\n", complex);
+    return 0;
+}
---
+++ DAA Solutions/insertion_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int complex = 0;
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            complex++;
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+        complex++;
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("The Complexity is: %d\n", complex);
+    return 0;
+}
