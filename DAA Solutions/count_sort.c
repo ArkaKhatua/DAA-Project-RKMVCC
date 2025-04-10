c2672904f5258f9d15c69883604d1d92363491f8
Arka Khatua <arkhakhatua@gmail.com>
2025-04-10 21:10:42 +0530

    Add files via upload

---
+++ DAA Solutions/count_sort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int complex = 0;
+int findMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+void countSort(int arr[], int n)
+{
+    int max = findMax(arr, n);
+    int count[max + 1];
+    int output[n];
+    for (int i = 0; i <= max; i++)
+    {
+        count[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        count[arr[i]]++;
+    }
+    for (int i = 1; i <= max; i++)
+    {
+        count[i] += count[i - 1];
+        complex++;
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
+        complex++;
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    countSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("The Complexity is: %d\n", complex);
+    return 0;
+}
